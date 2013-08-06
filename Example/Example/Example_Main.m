6918e5e78ce0d7bb21cc54a65b872b915054aa82
Xu Zan <arthur.de.valerie@gmail.com>
2013-08-07 00:23:57 +0800

    Create a new Objective-C project : Example.xcodeproj
    From this project, Learn and study how to do programming with Objective-C language in the Xcode development environment of Apple Mac OS X.

---
+++ Example/Example/Example_Main.m
@@ -0,0 +1,21 @@
+/*********************************************************************************************************
+ * Project      : Example
+ * Description  : This is an Example project programmed in Objective-C language. 
+ *                It is just a demo program for studying / learning purpose.
+ * --------------------------------------------------------------------------------------------------------
+ * File name    : Example_Main.m
+ * Description  : A simple example program, this file will implement the main() entry function.
+ * Created by   : Xu Zan
+ * Creation date: Wed.  Aug. 07, 2013
+ * Copyright(C)     2013    All rights reserved.
+ *
+ **********************************************************************************************************/
+
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, char *argv[])
+{
+    NSLog(@"Hello from Objective-C");
+    return 0;
+}
