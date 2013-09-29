214db39f2f6d3368f9ff261ff80bed119887a777
徐 贊 <arthur.de.valerie@gmail.com>
2013-09-30 00:06:24 +0800

    Study項目 ：學習研究Objective-C編程，該項目有只是一個用來學習研究用的示例程序。

--- Study/Study/main.m
+++ Study/Study/main.m
@@ -1,22 +1,81 @@
-//
-//  main.m
-//  Study
-//
-//  Created by 徐 贊 on 13-8-22.
-//  Copyright (c) 2013年 XUZAN. All rights reserved.
-//
+/*******************************************************************************************
+ * Project      : Study
+ * Description  : 學習研究 Objective-C編程，該項目只是一個用來學習研究用的示例程序。
+ * -----------------------------------------------------------------------------------------
+ * File name    : main.m
+ * Description  : This file implements the main() entry function.
+ * Creator      : XU ZAN   
+ * Creation date: Thur. Aug. 22, 2013
+ * Copyright(C)     2013    All rights reserved.
+ *
+ *******************************************************************************************/
+
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[])
-{
+/*=====================================================
+ * Functions prototype :
+ */
+long int calculateFactorial(int iValue);
+void myFunction(int a, int *b);
 
-    @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+
+/*=====================================================
+ * main() entry function :
+ */
+#if 0
+    int main(int argc, char *argv[])
+    {
+        int a = 5;
+    
+        long int lResult = calculateFactorial(a);
+    
+        NSLog(@"5! = %ld", calculateFactorial(5));
+        NSLog(@"10! = %ld", calculateFactorial(10));
+        NSLog(@"15! = %ld", calculateFactorial(15));
+        NSLog(@"20! = %ld", calculateFactorial(20));
+    
+        /*******************************/
+        return 0;
     }
-    return 0;
+#else
+    int main(int argc, char *argv[])
+    {
+        int a = 10;
+        int b = 10;
+        
+        myFunction(a, &b);
+        
+        NSLog(@"a = %d", a);
+        NSLog(@"b = %d", b);
+    /******************************/
+        return 0;
+    }
+#endif
+
+
+/*=====================================================
+ * Functions implementation :
+ */
+long int calculateFactorial(int iValue)
+{
+    long int iResult = 1;
+    
+    for (int i = 1; i <= iValue; i++)
+    {
+        iResult = iResult * i;
+    }
+    return iResult;
 }
 
+void myFunction(int a, int *b)
+{
+    a = 20;
+    *b = 20;
+    return;
+}
+
+
+/*
+ * END OF FILE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+ */
