4afbc7a1cc2b4b0159d65128f830757891e6c03c
徐 贊 <arthur.de.valerie@gmail.com>
2013-09-29 23:52:49 +0800

    創建，編寫一個簡單完整的Objective-C項目 ： Calculator
    它包含有多個Interface / implementation program-file-pair, 是一個完整的Startup項目。
    
    學習Objective-C編程便從此'Calculator' console project項目開始了。

---
+++ Calculator/Calculator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by 徐 贊 on 13-9-24.
+//  Copyright (c) 2013年 徐 贊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
