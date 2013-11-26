673e883b7cea9fdb3c3c9d829d62c5160bbe7337
徐 贊 <arthur.de.valerie@gmail.com>
2013-11-26 09:19:17 +0800

    Create_Class项目用来研究测试类方法和对象方法， 以及类的成员变量的属性。

---
+++ Create_Class/Create_Class/Testing.m
@@ -0,0 +1,12 @@
+/********************************************************************************************
+ * File name    : Testing.m
+ * Description  : 測試類， 對象的創建於使用
+ * Creator      : XU ZAN
+ * Creation date: Wed.  Oct. 23, 2013
+ * Copyright(C)     2013    All rights reserved.
+ *
+ ********************************************************************************************/
+
+#import <Foundation/Foundation.h>
+
+#import "Foo.h"
---
+++ Create_Class/Create_Class/main.m
@@ -0,0 +1,26 @@
+/*******************************************************************************************************
+ * Project      : Create_Class
+ * Description  : 類的創建 --> 接口 @interface   實現 @implementation
+ * -----------------------------------------------------------------------------------------------------
+ * File name    : main.m
+ * Description  : This file implements the main() entry function for current project.
+ * Creator      : XU ZAN
+ * Creation date: Sun.  Oct. 20, 2013
+ * Copyright(C)     2013    All rights reserved.
+ *
+ *******************************************************************************************************/
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
