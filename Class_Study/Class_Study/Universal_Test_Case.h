12b1accab809c0aa94581718faa842d325334094
徐 贊 <arthur.de.valerie@gmail.com>
2014-02-05 20:43:39 +0800

    引入獨立的測試用例， 調用執行測試的模塊 ： Universal_Test_Case

---
+++ Class_Study/Class_Study/Universal_Test_Case.h
@@ -0,0 +1,16 @@
+/**********************************************************************************************************************
+ * File name        : Universal_Test_Case.h
+ * Description      : 設置一個獨立模塊 Universal_Test_Case， 用來執行各個具體的測試用例， 
+ *                    而不用將各個測試用例函數添加到main()函數中, 增強各模塊的獨立性和程序結構清晰。
+ * Creator          : XU ZAN
+ * Creation date    : Wed.  Feb. 05, 2014
+ * Copyright(C)     2014    All rights reserved.
+ *
+ **********************************************************************************************************************/
+
+#ifndef UNIVERSAL_TEST_CASE_H
+#define UNIVERSAL_TEST_CASE_H
+
+    void Execute_Universal_Test_Case(void);
+
+#endif  /*  UNIVERSAL_TEST_CASE_H  */
---
+++ Class_Study/Class_Study/Universal_Test_Case.m
@@ -0,0 +1,18 @@
+/**********************************************************************************************************************
+ * File name        : Universal_Test_Case.m
+ * Description      : 調用執行各測試用例
+ * Creator          : XU ZAN
+ * Creation date    : Wed.  Feb. 05, 2014
+ * Copyright(C)     2014    All rights reserved.
+ *
+ **********************************************************************************************************************/
+
+
+#import "Universal_Test_Case.h"
+
+#import "ClassTest.h"
+
+void Execute_Universal_Test_Case(void)
+{
+        General_Test_Case();
+}
