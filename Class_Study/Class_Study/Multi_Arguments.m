8158171a39fc91af950386cb8b595dd7458b6ebc
徐 贊 <arthur.de.valerie@gmail.com>
2014-02-05 21:20:05 +0800

    增加 Multi_Arguments 模塊 及其測試用例函數。
    該模塊當前是空的， 待實現。

---
+++ Class_Study/Class_Study/Multi_Arguments.m
@@ -0,0 +1,17 @@
+/*******************************************************************************************************
+ * File name        : Multi_Arguments.m
+ * Description      : 研究與測試方法函數多參數輸入的問題
+ * Creator          : XU ZAN
+ * Creation date    : Wed.  Feb. 05, 2014
+ * Copyright(C)     2014    All rights reserved.
+ *
+ *******************************************************************************************************/
+
+
+#import "Multi_Arguments.h"
+
+@implementation MultiArguments
+
+
+
+@end
