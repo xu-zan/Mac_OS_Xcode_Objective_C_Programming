bb43308bdf1f1a09c250b2711475b91ccb64028a
Xu Zan <victorywell2003@163.com>
2014-12-01 01:12:20 +0800

    Create a new module "Dictionary_Object", to study how to program the NSDictionary & NSMutableDictionary class of objects.

---
+++ Foundation_Framework_Study/Foundation_Framework_Study/Dictionary_Object/Dictionary_Object.h
@@ -0,0 +1,15 @@
+/*********************************************************************************
+ * File name		: Dictionary_Object.h
+ * Description		: 研究字典對象的使用
+ * Creator			: XU ZAN
+ * Creation date	: Sun.	Nov. 30, 2014
+ * Copyright(C)		2014	All rights reserved.
+ *
+ *********************************************************************************/
+
+
+#ifndef DICTIONARY_OBJECT_H
+#define DICTIONARY_OBJECT_H
+
+
+#endif	/*  DICTIONARY_OBJECT_H  */
---
+++ Foundation_Framework_Study/Foundation_Framework_Study/Dictionary_Object/Dictionary_Object.m
@@ -0,0 +1,11 @@
+/*********************************************************************************
+ * File name		: Dictionary_Object.m
+ * Description		: 研究字典對象的使用
+ * Creator			: XU ZAN
+ * Creation date	: Sun.	Nov. 30, 2014
+ * Copyright(C)		2014	All rights reserved.
+ *
+ *********************************************************************************/
+
+
+#import "Dictionary_Object.h"
