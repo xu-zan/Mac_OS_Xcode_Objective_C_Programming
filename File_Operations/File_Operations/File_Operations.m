87e23a79665c3d6a7c523dce083cba90d2505da0
Xu Zan <victorywell2003@163.com>
2014-12-09 23:28:03 +0800

    Add 3 program files into current project. 	File_Operations_Main.m 	File_Operations.h 	File_Operations.m

---
+++ File_Operations/File_Operations/File_Operations.m
@@ -0,0 +1,18 @@
+/************************************************************************************************
+ * File name		: File_Operations.m
+ * Description		: Define the prototype of "File_Operations".
+ * Creator			: XU ZAN
+ * Creation date	: Tue.	Dec. 09, 2014
+ * Copyright(C)		2014	All rights reserved.
+ *
+ ************************************************************************************************/
+
+
+#import "File_Operations.h"
+
+
+@implementation File_Operations
+
+
+
+@end
---
+++ File_Operations/File_Operations/File_Operations_Main.m
@@ -0,0 +1,22 @@
+/***********************************************************************************************************
+ * Project			: File_Operations
+ * Description		: Learn and study the topic of File Operations in Objective-C programming.
+ * =========================================================================================================
+ * File name		: File_Operations_Main.c
+ * Description		: This file implements the main() portal function.
+ * Creator			: XU ZAN
+ * Creation date	: Sat.	Dec. 6, 2014
+ * Copyright(C)		2014	All rights reserved.
+ *
+ ***********************************************************************************************************/
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+	@autoreleasepool
+	{
+	
+		NSLog(@"\n");
+	}
+    return 0;
+}
