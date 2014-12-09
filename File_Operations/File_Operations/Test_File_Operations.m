6b9b6bc457c2f15546b86dc747958fb941b409b8
Xu Zan <victorywell2003@163.com>
2014-12-09 23:43:42 +0800

    Add a couple of declaration/implementation "Test_File_Operations".
    Used to define and implement the test cases for this project.

---
+++ File_Operations/File_Operations/Test_File_Operations.m
@@ -0,0 +1,17 @@
+/****************************************************************************
+ * File name		: Test_File_Operations.m
+ * Description		: Define and implement the test case functions for current
+ *					  "File_Operations" project.  Design the test cases here.
+ * Crreator			: XU ZAN
+ * Creation date	: Tue.	Dec. 09, 2014
+ * Copyright(C)		2014	All rights reserved.
+ *
+ ****************************************************************************/
+
+
+#import "Test_File_Operations.h"
+
+
+void General_Test_Cases(void)
+{
+}
