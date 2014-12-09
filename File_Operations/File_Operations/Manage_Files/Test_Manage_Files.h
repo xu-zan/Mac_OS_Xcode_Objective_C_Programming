0221446407928adc94751f359d983ffa5b56e73b
Xu Zan <victorywell2003@163.com>
2014-12-10 00:43:47 +0800

    增加兩個程序文件 Test_Manage_Files.h (.c)   用來測試驗證“Manage_Files”模塊用的。

---
+++ File_Operations/File_Operations/Manage_Files/Test_Manage_Files.h
@@ -0,0 +1,16 @@
+/***********************************************************************************
+ * File name		: Test_Manage_Files.h
+ * Description		: 測試驗證“Manage_Files”模塊。
+ * Creator			: XU ZAN
+ * Creation date	: Wed.	Dec. 10, 2014
+ * Copyright(C)		2014	All rights reserved.
+ *
+ ***********************************************************************************/
+
+
+#ifndef TEST_MANAGE_FILES_H
+#define TEST_MANAGE_FILES_H
+
+
+
+#endif	/*  TEST_MANAGE_FILES_H  */
---
+++ File_Operations/File_Operations/Manage_Files/Test_Manage_Files.m
@@ -0,0 +1,11 @@
+/***********************************************************************************
+ * File name		: Test_Manage_Files.m
+ * Description		: 測試驗證“Manage_Files”模塊。
+ * Creator			: XU ZAN
+ * Creation date	: Wed.	Dec. 10, 2014
+ * Copyright(C)		2014	All rights reserved.
+ *
+ ***********************************************************************************/
+
+
+#import "Test_Manage_Files.h"
