acbfb2f3112c741452ca2f808a85c431566980de
Xu Zan <victorywell2003@163.com>
2014-12-10 00:15:05 +0800

    Append a new independent module "Manage_Files" to implement how to manage the files by programming.

---
+++ File_Operations/File_Operations/Manage_Files/Manage_Files.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * File name		: Manage_Files.h
+ * Description		: The module prototype of "Manage_Files".
+ * Creator			: XU ZAN
+ * Creation date	: Tue.	Dec. 09, 2014
+ * Copyright(C)		2014	All rights reserved.
+ *
+ ******************************************************************************/
+
+
+#ifndef MANAGE_FILES_H
+#define MANAGE_FILES_H
+
+
+#endif	/*  MANAGE_FILES_H  */
---
+++ File_Operations/File_Operations/Manage_Files/Manage_Files.m
@@ -0,0 +1,11 @@
+/******************************************************************************
+ * File name		: Manage_Files.m
+ * Description		: The module implementation of "Manage_Files".
+ * Creator			: XU ZAN
+ * Creation date	: Tue.	Dec. 09, 2014
+ * Copyright(C)		2014	All rights reserved.
+ *
+ ******************************************************************************/
+
+
+#import "Manage_Files.h"
