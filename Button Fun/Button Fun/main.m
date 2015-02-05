54c4c12c23a462677bf1d0f5bb4f990176a586c4
Xu Zan <victorywell2003@163.com>
2015-02-05 09:26:37 +0800

    第一个iOS App项目  Hello_World
    按照 《Beginning iOS 6 Development  Exploring the iOS SDK   精通iOS開發》 一書， 學習研究iOS  Objective-C  App編程開發。

---
+++ Button Fun/Button Fun/main.m
@@ -0,0 +1,22 @@
+/********************************************************************************************************************************
+ * Project			: Button Fun
+ * Description		: My 1st GUI iOS application with interaction feature.
+ * ==============================================================================================================================
+ * File name		: main.m
+ * Description		: This file will implement the main() entry function for this "Button Fun" project.
+ * Creator			: XU ZAN
+ * Creation date	: Wed.	Feb. 04, 2015
+ * Copyright(C)		2015	All rights reserved.
+ *
+ ********************************************************************************************************************************/
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool
+	{
+		return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
---
+++ Hello_World_App/Hello_World_App/AppDelegate.m
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.m
+//  Hello_World_App
+//
+//  Created by Xu Zan on 15/1/21.
+//  Copyright (c) 2015年 Xu Zan. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+
+
+@implementation AppDelegate
+
+	- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+	{
+		// Insert code here to initialize your application
+	}
+
+	- (void)applicationWillTerminate:(NSNotification *)aNotification
+	{
+		// Insert code here to tear down your application
+	}
+
+@end
