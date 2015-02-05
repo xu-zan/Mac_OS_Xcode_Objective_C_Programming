49600aafa62a08f2f0d7585edee1d616473ad444
Xu Zan <victorywell2003@163.com>
2015-02-05 09:37:33 +0800

    Add source code files for "Hello_World.app"  iOS App project.
    These program files were generated automatically by Xcode iOS program template.

---
+++ Hello_World/Hello_World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello_World
+//
+//  Created by Xu Zan on 15/1/28.
+//  Copyright (c) 2015年 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello_World/Hello_World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello_World
+//
+//  Created by Xu Zan on 15/1/28.
+//  Copyright (c) 2015年 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
